49b763b607daa333f96beb12f1434b668f752802
Hlodr2 <ohi10@outlook.com>
2026-05-25 14:21:45 -0400

    water is obtainable

--- src/main/java/awanderingmuslim/potassiumwater/PotassiumWater.java
+++ src/main/java/awanderingmuslim/potassiumwater/PotassiumWater.java
@@ -4,6 +4,12 @@ import awanderingmuslim.potassiumwater.item.ModItemGroups;
 import awanderingmuslim.potassiumwater.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,12 +23,62 @@ public class PotassiumWater implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
         ModItems.initialize();
         ModItemGroups.initalize();
-        LOGGER.info("Hello Fabric world!");
+
+        // Loot drops for our water begin here
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
+
+            if (BuiltInLootTables.IGLOO_CHEST.equals(key)) {
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .add(LootItem.lootTableItem(ModItems.POTASSIUM_WATER)
+                                .setWeight(100)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 3.0F))
+                                ));
+
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+
+            if (BuiltInLootTables.JUNGLE_TEMPLE.equals(key)) {
+
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .add(LootItem.lootTableItem(ModItems.POTASSIUM_WATER)
+                                .setWeight(75)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 5.0F))
+                                ));
+
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+
+            if (BuiltInLootTables.ANCIENT_CITY.equals(key)) {
+
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .add(LootItem.lootTableItem(ModItems.POTASSIUM_WATER)
+                                .setWeight(60)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 15.0F))
+                                ));
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+
+
+            if (BuiltInLootTables.END_CITY_TREASURE.equals(key)) {
+
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .add(LootItem.lootTableItem(ModItems.POTASSIUM_WATER)
+                                .setWeight(100)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(5.0F, 10.0F))
+                                ));
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+
+        });
+        //Lootdrops for our water end here
+
+        LOGGER.info("This is" + MOD_ID);
 
     }
 }
--- src/main/java/awanderingmuslim/potassiumwater/entity/custom/PotassiumWaterProjectile.java
+++ src/main/java/awanderingmuslim/potassiumwater/entity/custom/PotassiumWaterProjectile.java
@@ -1,19 +1,12 @@
 package awanderingmuslim.potassiumwater.entity.custom;
 
-import net.minecraft.core.particles.ItemParticleOption;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.Blaze;
-import net.minecraft.world.entity.projectile.throwableitemprojectile.Snowball;
 import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 
 public class PotassiumWaterProjectile extends ThrowableItemProjectile {
@@ -34,20 +27,6 @@ public class PotassiumWaterProjectile extends ThrowableItemProjectile {
         return Items.SNOWBALL;
     }
 
-    private ParticleOptions getParticle() {
-        ItemStack itemStack = this.getItem();
-        return itemStack.isEmpty() ? ParticleTypes.ITEM_SNOWBALL : new ItemParticleOption(ParticleTypes.ITEM, itemStack);
-    }
-
-    @Override
-    public void handleEntityEvent(byte b) {
-        if (b == 3) {
-            ParticleOptions particleOptions = this.getParticle();
-            for (int i = 0; i < 8; ++i) {
-                this.level().addParticle(particleOptions, this.getX(), this.getY(), this.getZ(), 0.0, 0.0, 0.0);
-            }
-        }
-    }
 
     @Override
     protected void onHit(HitResult hitResult) {
